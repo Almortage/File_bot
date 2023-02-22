91ebf95724609f3e92e1d92f6795b2171a174386
Zein <122434261+zeinn1@users.noreply.github.com>
2023-02-22 19:21:15 +0200

    Update ذاتية.py

--- joker/plugins/ذاتية.py
+++ joker/plugins/ذاتية.py
@@ -1,6 +1,7 @@
-from joker import *
 from joker import l313l
-from ..sql_helper.globals import gvarstatus
+from ..sql_helper.globals import addgvar, delgvar, gvarstatus
+import os
+from joker import *
 
 @l313l.on(admin_cmd(pattern="(جلب الصورة|جلب الصوره|ذاتيه|ذاتية|حفظ)"))
 async def dato(event):
@@ -14,8 +15,41 @@ async def dato(event):
         caption=f"""
 - تـم حفظ الصـورة بنجـاح ✓ 
 - غير مبري الذمه اذا استخدمت الامر للابتزاز
-- CH: @S_EG_P
-- Dev: @iiqllll
+- Dev: @devpokemon
   """,
     )
     await event.delete()
+#By @jepthon For You 🌹
+@l313l.on(admin_cmd(pattern="(الذاتية تشغيل|ذاتية تشغيل)"))
+async def reda(event):
+    if gvarstatus ("savepicforme"):
+        return await edit_delete(event, "**᯽︙حفظ الذاتيات مفعل وليس بحاجة للتفعيل مجدداً **")
+    else:
+        addgvar("savepicforme", "reda")
+        await edit_delete(event, "**᯽︙تم تفعيل ميزة حفظ الذاتيات بنجاح ✓**")
+ 
+@l313l.on(admin_cmd(pattern="(الذاتية تعطيل|ذاتية تعطيل)"))
+async def Reda_Is_Here(event):
+    if gvarstatus ("savepicforme"):
+        delgvar("savepicforme")
+        return await edit_delete(event, "**᯽︙تم تعطيل حفظت الذاتيات بنجاح ✓**")
+    else:
+        await edit_delete(event, "**᯽︙انت لم تفعل حفظ الذاتيات لتعطيلها!**")
+
+
+@l313l.ar_cmd(incoming=True)
+async def reda(event):
+    if gvarstatus ("savepicforme"):
+        if event.is_private:
+            if event.media and event.media_unread:
+                pic = await event.download_media()
+                await bot.send_file(
+                "me",
+                pic,
+                caption=f"""
+                - تـم حفظ الصـورة بنجـاح ✓ 
+                - غير مبري الذمه اذا استخدمت الامر للابتزاز                
+                - Dev: @devpokemon
+                """,
+                )
+                os.remove(pic)
